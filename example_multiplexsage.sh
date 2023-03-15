ea02417721b8bf1ba4776e2cead2afc5ef1ac815
Luca Gallo <119935568+lgajo@users.noreply.github.com>
2023-03-15 11:18:29 +0100

    Edit example script

--- example_multiplexsage.sh
+++ example_multiplexsage.sh
@@ -1,1 +1,1 @@
-python -m multiplexsage.unsupervised_train --train_prefix ./Graphs/arxiv/multi --model muliplexsage_mean --max_total_steps 1000 --validate_iter 10 --n_layers 2 --n_try 1
+python -m multiplexsage.unsupervised_train --train_prefix ./Graphs/arxiv/multi --model muliplexsage_mean --max_total_steps 1000 --validate_iter 10 --n_layers 13 --n_try 1
